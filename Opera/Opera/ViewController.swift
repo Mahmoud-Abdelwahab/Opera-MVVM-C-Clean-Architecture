2bda7f5aa46daa753401becbd3d09ad85467c174
“Mahmoud-Abdelwahab” <mahmoudabdelwahab199390@gmail.com>
2021-06-17 16:39:12 +0200

    init project

---
+++ Opera/Opera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Opera
+//
+//  Created by Mahmoud Abdul-Wahab on 17/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
