8a57769116bb024fdfd05a8a1709674103a35c6f
“Mahmoud-Abdelwahab” <mahmoudabdelwahab199390@gmail.com>
2021-06-19 09:42:28 +0200

    commiting no playing design

---
+++ Opera/Opera/App/Scenes/NowPlaying/NowPlayingCell/NowPlayingCell.swift
@@ -0,0 +1,22 @@
+//
+//  NowPlayingCell.swift
+//  Opera
+//
+//  Created by Mahmoud Abdul-Wahab on 19/06/2021.
+//
+
+import UIKit
+
+class NowPlayingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
--- Opera/Opera/App/Scenes/NowPlaying/NowPlayingVC.swift
+++ Opera/Opera/App/Scenes/NowPlaying/NowPlayingVC.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class NowPlayingVC: BaseViewController<NowPlayingVM> {
 
+    @IBOutlet weak var nowPlayingTableVIew: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
